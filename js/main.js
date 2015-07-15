1d51625d30605890c53db6a05939fd41a1a671fb
jaime171 <jaimesalvador.simental@gmail.com>
2015-07-15 00:28:33 -0500

    Profiles pictures

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(function(){
-	$("h1").fadeIn(2000);
+	// $("h1").fadeIn(2000);
+
+
+	var $window = $(window);
+	var scrollTime = 1.2;
+	var scrollDistance = 170;
+
+	$window.on("mousewheel DOMMouseScroll", function(event){
+
+		event.preventDefault();	
+
+		var delta = event.originalEvent.wheelDelta/100 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,
+				overwrite: 15							
+			});
+
+	});
 });
