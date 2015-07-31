bbe94760604ab8b4a5018d31d796628cbc9ea0ec
jaimem <jaimem@redman.com>
2015-07-31 18:39:38 -0500

    Photo added

--- js/main.js
+++ js/main.js
@@ -1,24 +1,107 @@
 $(function(){
-	// $("h1").fadeIn(2000);
+	//$(".section-menu").fadeIn(2500);
+var butonCount = 1;
+	$(".section-menu").click(function(){
+		var lightbox = $(".lightbox-menu"),
+		firstBar = $("#line-1"),
+		secondBar = $("#line-2"),
+		thirdBar = $("#line-3");
+		$(this).css('z-index', 1000);
+		$(this).css({"opacity": 1});
+		// lightbox.animate({width:'toggle'},800);
+		lightbox.slideDown(800);
+		// lightbox.fadeIn(800);
+		firstBar.addClass("section-menu-rotation section-menu-time ");
+		thirdBar.addClass("section-menu-rotation-back section-menu-time ");
+		secondBar.fadeOut("fast");
+		butonCount++;
+		if(butonCount > 2){
+			butonCount = 2;
+			lightbox.slideUp(800);
+			// lightbox.fadeOut(800);
+			firstBar.toggleClass("section-menu-rotation section-menu-time ");
+			thirdBar.toggleClass("section-menu-rotation-back section-menu-time ");
+			secondBar.fadeIn("slow");
+			butonCount--;
+		}
+	
+
+		
 
 
-	var $window = $(window);
-	var scrollTime = 1.2;
-	var scrollDistance = 170;
 
-	$window.on("mousewheel DOMMouseScroll", function(event){
 
-		event.preventDefault();	
 
-		var delta = event.originalEvent.wheelDelta/100 || -event.originalEvent.detail/3;
-		var scrollTop = $window.scrollTop();
-		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
 
-		TweenMax.to($window, scrollTime, {
-			scrollTo : { y: finalScroll, autoKill:true },
-				ease: Power1.easeOut,
-				overwrite: 15							
-			});
 
-	});
+
+
+
+
+
+
+
+
+
+
+
+			// switch ($(".section-menu[id='1']")){
+
+			// }
+			// if($(".section-menu[id='1']")){
+			// 	$(this).css('z-index', 1000);
+			// 	$(this).css({"opacity": 1});
+			// 	lightbox.slideDown(800);
+			// 	firstBar.addClass("section-menu-rotation section-menu-time ");
+			// 	thirdBar.addClass("section-menu-rotation-back section-menu-time ");
+			// 	secondBar.fadeOut("fast");
+			// 	butonCount++
+			// 	$(".section-menu").attr("id",butonCount);
+			// 	// console.log($(".section-menu").attr("id", "div#+butonCount"));
+			// } else if ($(".section-menu[id='2']")){
+			// 	lightbox.slideup(800);
+			// }
+			// if(butonCount > 2){
+			// 	butonCount = 1;
+			// }
+			
+
+		
+		}); //function 
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onscroll = (function(e) {
+//     parallaxScroll();
+// });
+
+
+// function parallaxScroll() {
+//     var scrollLeft = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
+//     var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+//     var bgdiv = document.getElementById('parallax');
+//     bgdiv.style.top = -(scrollTop * 0.1) + 'px';
+// }
